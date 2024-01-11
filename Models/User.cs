25714655e8446f2448bb1e3f79537efa0444f8a8
Yehor <egkoshop@gmail.com>
2024-01-11 22:05:28 +0200

    Added class UserAddress

--- Models/User.cs
+++ Models/User.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        public string City { get; set; }
+        public UserAddress Address { get; set; }
         public DateTime BirthdayDate { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
