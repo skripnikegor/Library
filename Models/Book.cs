19f6341b97de974c9160ccebcf5e2ceed0584597
Yehor <egkoshop@gmail.com>
2024-01-11 21:56:24 +0200

    Added class Book

---
+++ Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Authtor { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+    }
+}
